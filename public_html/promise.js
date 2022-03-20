31946f2ec790c7e518d953eb4ee5a228df6ba794
twent <twentprod@gmail.com>
2022-03-20 18:12:39 +0300

    fix

--- public_html/promise.js
+++ public_html/promise.js
@@ -1,6 +1,4 @@
 async function task(x) { 
     const checkAge = age => new Promise((res, rej) => age < 18 ? rej('no') : res('yes'));
-    checkAge(x)
-        .then(result => { return result } )
-        .catch(error => { return error});
+    return checkAge(x);
 }
