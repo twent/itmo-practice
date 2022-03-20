406b6b153951c2e1b8f3b401a03a337de670816f
twent <twentprod@gmail.com>
2022-03-20 20:00:05 +0300

    Update promise.js

--- public_html/promise.js
+++ public_html/promise.js
@@ -1,3 +1,7 @@
-const task = x => {
+// const task = x => {
+//     return new Promise((res, rej) => x < 18 ? rej('no') : res('yes'));
+// };
+
+function task(x) { 
     return new Promise((res, rej) => x < 18 ? rej('no') : res('yes'));
-}
+};
